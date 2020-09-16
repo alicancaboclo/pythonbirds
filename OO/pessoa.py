218070ec979d3aebbf4f48d3dd8a5211d596de3d
alicancaboclo <69983359+alicancaboclo@users.noreply.github.com>
2020-09-16 19:53:31 -0300

    criando o metodo comprimentar

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,2 +1,10 @@
-class Pessoa:      #lenbra uma execusao de uma função
-    pass            #uma forma de criar tipos personalisados
+class Pessoa:                           #atributo=metodo é função pertencer a# uma classer senpre esta conectada a um objeto
+    def cumplimentar(self):             #criando o metodp comprimentar #lenbra uma execusao de uma função
+        return f' ola {id(self)}'
+if __name__ == '__name__':
+    p = Pessoa()
+    print(Pessoa.cumplimentar())
+    print(id(p))
+    print(p.cumplimentar())
+
+
