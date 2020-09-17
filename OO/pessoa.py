779c7736a20ba3a6579870d1dd92ab935a47c0fa
alicancaboclo <69983359+alicancaboclo@users.noreply.github.com>
2020-09-16 21:07:23 -0300

    atributo de metodo de instância é criado com o metodo __init__, que criar objetos com teg self

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,10 +1,18 @@
 class Pessoa:                           #atributo=metodo é função pertencer a# uma classer senpre esta conectada a um objeto
-    def cumplimentar(self):             #criando o metodp comprimentar #lenbra uma execusao de uma função
+    def __init__(self, nota='9', idade='18', nome=None):       #atributo de dados(metodor(__init__)(defini um retorno vazio=None)
+        self.nome = nome                          #nome do obejeto  #a tributo de metodp de intantência é criado cometodo __init__
+        self.idade = idade
+        self.nota = nota
+
+
+    def cumplimentar(self):                       #criando o metodp comprimentar #lenbra uma execusao de uma função
         return f' ola {id(self)}'
 if __name__ == '__name__':
-    p = Pessoa()
-    print(Pessoa.cumplimentar())
+    p = Pessoa('alikan')
+    print(Pessoa.cumplimentar(p))
     print(id(p))
     print(p.cumplimentar())
-
+    p.nome = 'alikan'
+    print(p.nome)
+    print(p.idade)
 
