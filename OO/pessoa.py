842dd0f3a1ee7a266bb402b8eed3d1b8f19f535d
alicancaboclo <69983359+alicancaboclo@users.noreply.github.com>
2020-09-16 22:22:21 -0300

    criando um atributo complexo dentro da class passando por uma def(*filhos) depois chamando a list>(self.filhos=list(filhos)<criando objetos< >para passar como atribudo da list >(alikan=Pessoa(nome='alikan')<.

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,18 +1,20 @@
 class Pessoa:                           #atributo=metodo é função pertencer a# uma classer senpre esta conectada a um objeto
-    def __init__(self, nota='9', idade='18', nome=None):       #atributo de dados(metodor(__init__)(defini um retorno vazio=None)
-        self.nome = nome                          #nome do obejeto  #a tributo de metodp de intantência é criado cometodo __init__
+    def __init__(self,*filhos, nota='9', idade='18', nome=None):       #atributo de dados(metodor(__init__)(defini um retorno vazio=None) #a tributo de metodp de intantência é criado cometodo __init__
+        self.nome = nome                          #nome do obejeto
         self.idade = idade
         self.nota = nota
-
+        self.filhos = list(filhos)               # criação de list para numero complexo
 
     def cumplimentar(self):                       #criando o metodp comprimentar #lenbra uma execusao de uma função
         return f' ola {id(self)}'
 if __name__ == '__name__':
-    p = Pessoa('alikan')
-    print(Pessoa.cumplimentar(p))
-    print(id(p))
-    print(p.cumplimentar())
-    p.nome = 'alikan'
-    print(p.nome)
-    print(p.idade)
-
+    alikan = Pessoa(nome='alikan')                  #obejeto do tipo complexo alikan
+    caboclo = Pessoa(alikan,nome='silva')           #passando como atributo alikan
+    print(Pessoa.cumplimentar(caboclo))
+    print(id(caboclo))
+    print(caboclo.cumplimentar())
+    print(caboclo.nome)
+    print(caboclo.idade)
+    for filho in caboclo.filhos:           #
+        print(filho.nome)
+    print(caboclo.filho)                    #
