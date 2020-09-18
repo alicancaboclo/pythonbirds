f0b1ccdb9e6a31149ad5e49ac86bc1741b4f4ebe
alicancaboclo <69983359+alicancaboclo@users.noreply.github.com>
2020-09-17 22:08:58 -0300

    criando atributo de class sem inserir na def mas podendo ser chamado e alterado dentro do atributo

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,20 +1,33 @@
-class Pessoa:                           #atributo=metodo é função pertencer a# uma classer senpre esta conectada a um objeto
+class Pessoa:                                                          #atributo=metodo é função pertencer a# uma classer senpre esta conectada a um objeto
+    olhos=2                                                            #criando atributo de class<> mas pode chamar na def sem locar memoria
+                                                                       #os atributo de class (olhos0 pode chama dentro da def(objetos) sem declara (__init__)
     def __init__(self,*filhos, nota='9', idade='18', nome=None):       #atributo de dados(metodor(__init__)(defini um retorno vazio=None) #a tributo de metodp de intantência é criado cometodo __init__
-        self.nome = nome                          #nome do obejeto
+        self.nome = nome                                               #nome do obejeto
         self.idade = idade
         self.nota = nota
-        self.filhos = list(filhos)               # criação de list para numero complexo
+        self.filhos = list(filhos)                                     # criação de list para numero complexo
 
-    def cumplimentar(self):                       #criando o metodp comprimentar #lenbra uma execusao de uma função
+    def cumplimentar(self):                                            #criando o metodp comprimentar #lenbra uma execusao de uma função
         return f' ola {id(self)}'
 if __name__ == '__name__':
-    alikan = Pessoa(nome='alikan')                  #obejeto do tipo complexo alikan
-    caboclo = Pessoa(alikan,nome='silva')           #passando como atributo alikan
+    alikan = Pessoa(nome='alikan')                                      #obejeto do tipo complexo alikan
+    caboclo = Pessoa(alikan,nome='silva')                               #passando como atributo alikan
     print(Pessoa.cumplimentar(caboclo))
     print(id(caboclo))
     print(caboclo.cumplimentar())
     print(caboclo.nome)
     print(caboclo.idade)
-    for filho in caboclo.filhos:           #
+    for filho in caboclo.filhos:                                       # FOR= para filho insira no objeto com o atributo e passe filhos.nome(o atributo dentro da list)
         print(filho.nome)
-    print(caboclo.filho)                    #
+    print(caboclo.filhos)
+    del caboclo.filhos                                                  #deletando atributo dinâmico
+    caboclo.sobrenome='kill'                                            #criando um atribudo dinâmico sem definir na def (o uso só onde foi criado)
+    print(caboclo.sobrenome)                                            # o atribudo não modificar os objetos da mesma class
+    caboclo.olhos= 1                                                    #mudando o valor do atributo do objeto sem autera a class
+    print(caboclo.__dict__)                                             # discrição complete ou listando todos da objetos e atribudo da class
+    print(caboclo.__dict__)
+    Pessoa.olhos=3                                                      #auterandoo valor atributo de class
+    print(Pessoa.olhos)                                                 #acessando atributo de class(olhos)
+    print(caboclo.olhos)                                                ### se o atributo de class for ingual a para todos objetos nao cria objeto mas pode
+    print(alikan.olhos)                                                 ###  ser auterado dentro do objeto
+    print(id(Pessoa.olhos), id(caboclo.olhos),id(alikan.olhos))
